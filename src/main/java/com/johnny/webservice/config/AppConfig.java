b99fdb2a7ccddf300c42ad8b83f46cb03975d528
Olugbodi Johnny <olugbodijohnny@live.com>
2022-10-09 21:38:56 +0100

    updated configs

--- src/main/java/com/johnny/webservice/config/AppConfig.java
+++ src/main/java/com/johnny/webservice/config/AppConfig.java
@@ -6,13 +6,10 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
-import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 
 import javax.servlet.Servlet;
 
@@ -37,35 +34,45 @@ public class AppConfig {
                 "/johnny/web-service-practice/*");
     }
 
-    @Bean(name = "SmsNotificationManagerService")
-    public Wsdl11Definition smsNotificationManagerServiceWsdl11Definition() {
-        SimpleWsdl11Definition wsdl11Definition =
-                new SimpleWsdl11Definition();
-        wsdl11Definition
-                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_manager_interface_2_3.wsdl"));
+//    @Bean(name = "SmsNotificationManagerService")
+//    public Wsdl11Definition smsNotificationManagerServiceWsdl11Definition() {
+//        SimpleWsdl11Definition wsdl11Definition =
+//                new SimpleWsdl11Definition();
+//        wsdl11Definition
+//                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_manager_interface_2_3.wsdl"));
+//
+//        return wsdl11Definition;
+//    }
 
-        return wsdl11Definition;
-    }
-
-    @Bean(name = "SmsNotificationService")
-    public Wsdl11Definition smsNotificationServiceWsdl11Definition() {
-        SimpleWsdl11Definition wsdl11Definition =
-                new SimpleWsdl11Definition();
-        wsdl11Definition
-                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_service_2_2.wsdl"));
-        return wsdl11Definition;
-    }
-
-    @Bean(name = "SendSmsService")
-    public Wsdl11Definition sendSmsServiceWsdl11Definition() {
-        SimpleWsdl11Definition wsdl11Definition =
-                new SimpleWsdl11Definition();
-        wsdl11Definition
-                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_service_2_2.wsdl"));
-        return wsdl11Definition;
-    }
-
-    @Bean
+//    @Bean(name = "SmsNotificationService")
+//    public Wsdl11Definition smsNotificationServiceWsdl11Definition() {
+//        SimpleWsdl11Definition wsdl11Definition =
+//                new SimpleWsdl11Definition();
+//        wsdl11Definition
+//                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_service_2_2.wsdl"));
+//        return wsdl11Definition;
+//    }
+//
+//    @Bean(name = "SendSmsService")
+//    public Wsdl11Definition sendSmsServiceWsdl11Definition() {
+//        SimpleWsdl11Definition wsdl11Definition =
+//                new SimpleWsdl11Definition();
+//        wsdl11Definition
+//                .setWsdl(new ClassPathResource("/wsdl/parlayx_sms_notification_service_2_2.wsdl"));
+//        return wsdl11Definition;
+//    }
+//
+//    @Bean(name = "USSDNotificationManagerService")
+//    public Wsdl11Definition uSSDNotificationManagerServiceWsdl11Definition() {
+//        SimpleWsdl11Definition wsdl11Definition =
+//                new SimpleWsdl11Definition();
+//        wsdl11Definition
+//                .setWsdl(new ClassPathResource("/wsdl/osg_ussd_notification_manager_service_1_0.wsdl"));
+//        return wsdl11Definition;
+//    }
+//
+    @Bean("smsNotificationManagerServiceJaxb2Marshaller")
+//    @Qualifier("smsNotificationManagerServiceJaxb2Marshaller")
     Jaxb2Marshaller smsNotificationManagerServiceJaxb2Marshaller() {
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
         jaxb2Marshaller
@@ -73,7 +80,8 @@ public class AppConfig {
         return jaxb2Marshaller;
     }
 
-    @Bean
+    @Bean("smsNotificationServiceJaxb2Marshaller")
+//    @Qualifier("smsNotificationServiceJaxb2Marshaller")
     Jaxb2Marshaller smsNotificationServiceJaxb2Marshaller() {
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
         jaxb2Marshaller
@@ -81,13 +89,22 @@ public class AppConfig {
         return jaxb2Marshaller;
     }
 
-    @Bean
+    @Bean("sendSmsServiceJaxb2Marshaller")
     Jaxb2Marshaller sendSmsServiceJaxb2Marshaller() {
         Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
         jaxb2Marshaller
                 .setContextPath("com.johnny.webservice.schema.parlayx.sms.send.v2_2.local");
         return jaxb2Marshaller;
     }
+
+//
+//    @Bean
+//    Jaxb2Marshaller uSSDNotificationManagerServiceJaxb2Marshaller() {
+//        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+//        jaxb2Marshaller
+//                .setContextPath("com.johnny.webservice.schema.osg.ussd.notification_manager.v1_0.local");
+//        return jaxb2Marshaller;
+//    }
 
     @Bean
     public WebServiceTemplate webServiceTemplate() {
