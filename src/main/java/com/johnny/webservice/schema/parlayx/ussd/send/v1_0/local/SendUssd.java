d695c9a03dd15f39404eda6ce702f54c9b3c5c3c
Olugbodi Johnny <olugbodijohnny@live.com>
2022-10-11 21:37:19 +0100

    added annotations for send ussd POJOs

--- src/main/java/com/johnny/webservice/schema/parlayx/ussd/send/v1_0/local/SendUssd.java
+++ src/main/java/com/johnny/webservice/schema/parlayx/ussd/send/v1_0/local/SendUssd.java
@@ -8,10 +8,7 @@
 
 package com.johnny.webservice.schema.parlayx.ussd.send.v1_0.local;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -42,6 +39,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "sendUssd", propOrder = {
     "msgType",
--- src/main/java/com/johnny/webservice/schema/parlayx/ussd/send/v1_0/local/SendUssdAbort.java
+++ src/main/java/com/johnny/webservice/schema/parlayx/ussd/send/v1_0/local/SendUssdAbort.java
@@ -8,10 +8,7 @@
 
 package com.johnny.webservice.schema.parlayx.ussd.send.v1_0.local;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.*;
 
 
 /**
@@ -36,6 +33,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  * 
  */
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "sendUssdAbort", propOrder = {
     "senderCB",
